9538687db07f8326438ae7ad7998e201718f5d2a
Ubuntu <puruiw@yahoo.com>
2021-06-03 04:47:32 +0000

    A slightly more robust random move.

--- demo/random_move.py
+++ demo/random_move.py
@@ -3,22 +3,30 @@ import time
 
 from my_robot.xiao_r_robot import XiaoRRobot
 
-def random_move():
 
+def move_till_hit(r, max_time):
+  start = time.time()
+  r.start_forward(0.7)
+  while time.time() < start + max_time:
+    distance = r.sonar.get_distance()
+    print(distance)
+    if distance < 0.5:
+      break
+    time.sleep(0.01)
+  r.stop()
+
+
+def random_move():
   r = XiaoRRobot()
   r.init()
  
   while True:
     try:
-      r.start_forward(0.7)
-      while True:
-        distance = r.sonar.get_distance()
-        print(distance)
-        if distance < 0.5:
-          break
-        time.sleep(0.01)
-      r.stop()
-      r.start_turn_left(0.3)
+      move_till_hit(r, 2.0)
+      if random.random() < 0.5:
+        r.start_turn_left(1.0)
+      else:
+        r.start_turn_right(1.0)
       time.sleep(random.random() * 0.5 + 0.1)
     except KeyboardInterrupt:
       r.cleanup()
